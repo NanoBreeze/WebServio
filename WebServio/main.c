cd6b81a8ea545f5f9247772ff8e3b1008127ca06
NanoBreeze <dreambigbebigger@gmail.com>
2017-03-29 18:23:21 -0400

    Added receiving information

--- WebServio/main.c
+++ WebServio/main.c
@@ -8,13 +8,18 @@
 #include <netdb.h>
 
 
+#include <pthread.h>
 
+#define PORT "3002"
 #define BACKLOG 20
+
+
+char buffer[100000];
+
 
 int main()
 {
 
-   printf("Hello world!\n");
 
     struct addrinfo hints;
     struct addrinfo *result;
@@ -26,7 +31,7 @@ int main()
     hints.ai_flags = AI_PASSIVE;
 
     int status;
-    if ((status = getaddrinfo(NULL, "3002", &hints, &result)) != 0) {
+    if ((status = getaddrinfo(NULL, PORT, &hints, &result)) != 0) {
         printf("Error with getaddrinfo: %s\n", gai_strerror(status));
         return;
     }
@@ -34,6 +39,13 @@ int main()
     int fileDescriptor;
     if ((fileDescriptor = socket(result->ai_family, result->ai_socktype, result->ai_protocol)) == -1) {
         printf("Error with fileDescriptor: %s\n", strerror(errno));
+        return;
+    }
+
+    int yes = 1;
+    int setSockErrorCode;
+    if ((setSockErrorCode = setsockopt(fileDescriptor, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes)) == -1) {
+        printf("Error with setting setsockopt to allow reusing port: %s\n", strerror(errno));
         return;
     }
 
@@ -52,22 +64,30 @@ int main()
 
     printf("about to wait\n");
 
-    int acceptedFileDesciptor;
-    socklen_t acceptedFileDesciptorLength = sizeof acceptedFileDesciptor;
+    int acceptedFileDescriptor;
+    socklen_t acceptedFileDescriptorLength = sizeof acceptedFileDescriptor;
     struct sockaddr_storage acceptedAddr;
-    if ((acceptedFileDesciptor = accept(fileDescriptor, (struct sockaddr*) &acceptedAddr, &acceptedFileDesciptorLength)) == -1) {
+    if ((acceptedFileDescriptor = accept(fileDescriptor, (struct sockaddr*) &acceptedAddr, &acceptedFileDescriptorLength)) == -1) {
         printf("Error with accepting: %s\n", strerror(errno));
         return;
     }
 
-    printf("%d", acceptedFileDesciptor);
+    printf("%d", acceptedFileDescriptor);
 
-
-
-
-
-
-
+    int bytesReceived = 0;
+    if ( (bytesReceived = recv(acceptedFileDescriptor, buffer, sizeof buffer, 0)) <= 0) {
+        //connection closed
+        if (bytesReceived == 0) {
+            printf("Client closed connection");
+        }
+        else {
+            printf("Error with receiving information: %s\n", strerror(errno));
+        }
+        close(acceptedFileDescriptor);
+    }
+    else {
+        printf("Received data: \n%s", buffer);
+    }
 
 
     return 0;
